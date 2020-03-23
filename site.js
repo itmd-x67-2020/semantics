0fdd74612ac59a98f9a20ed2c8247fb14eb839fb
Karl Stolley <karl.stolley@gmail.com>
2020-03-23 16:35:03 -0500

    Add an unobtrusive script to handle form submission

---
+++ site.js
@@ -0,0 +1,6 @@
+var form = document.querySelector('#user-information');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  console.log('OMG form submitted!!1!');
+});
